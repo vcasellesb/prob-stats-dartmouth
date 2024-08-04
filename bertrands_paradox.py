65e60fc94d310f052a8598b9202f531527e64a08
vcasellesb <vcasellesb@uoc.edu>
2024-08-04 14:17:01 +0200

    Implemented modes 2 and 3

--- bertrands_paradox.py
+++ bertrands_paradox.py
@@ -1,4 +1,51 @@
-
+import random
+from typing import Tuple
+import numpy as np
 
 # Example 2.6 https://math.dartmouth.edu/~prob/prob/prob.pdf
 
+def rectangular_coord() -> Tuple[float, float]:
+    x = random.uniform(-1, 1) # actually, it doesn't matter if its [0, 1] or [-1, 1]
+    y = random.uniform(-1, 1)
+    return x, y
+
+def polar_coord_midpoint():
+    r = random.uniform(0, 1)
+    return r
+
+def polar_coord_endpoints():
+    alpha = random.uniform(0, 2*np.pi)
+    return alpha
+
+def main(niters: int, mode: int) -> None:
+    res = 0
+    true_iters = 0
+    for i in range(niters):
+        match mode:
+            case 1:
+                x, y = rectangular_coord()
+                if (x**2) + (y**2) > 1:
+                    # we are outside circle
+                    continue
+                decision = (2 * np.sqrt((1 - ((x**2) + (y**2))))) > np.sqrt(3)
+                res += decision
+                true_iters += 1  
+            case 2:
+                r = polar_coord_midpoint()
+                decision = (2 * np.sqrt(1 - (r**2))) > np.sqrt(3)
+                res += decision
+            case 3:
+                alpha = polar_coord_endpoints()
+                decision = (np.sqrt(2 - 2 * np.cos(alpha))) > np.sqrt(3)
+                res += decision
+
+            case _:
+                raise ValueError
+    
+    return res/true_iters if mode == 1 else res/niters
+
+
+if __name__ == "__main__":
+
+    results = main(10000, mode=3)
+    print(results)
