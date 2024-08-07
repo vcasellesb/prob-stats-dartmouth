22a8e7c8c06c11083b03857908a9e440d49ceb85
vcasellesb <vcasellesb@uoc.edu>
2024-08-07 21:07:36 +0200

    added some links

--- bertrands_paradox.py
+++ bertrands_paradox.py
@@ -3,6 +3,8 @@ from typing import Tuple
 import numpy as np
 
 # Example 2.6 https://math.dartmouth.edu/~prob/prob/prob.pdf
+# Note: the length of a chord is given by 2 * √(r^2 - d^2) where d is the line that
+# bisects the chord
 
 def rectangular_coord() -> Tuple[float, float]:
     x = random.uniform(-1, 1) # actually, it doesn't matter if its [0, 1] or [-1, 1]
@@ -20,6 +22,8 @@ def polar_coord_endpoints():
 def main(niters: int, mode: int) -> None:
     res = 0
     true_iters = 0
+    # the why behind the differences between methods are beautifully explained here
+    # https://www.uio.no/studier/emner/matnat/math/MAT4010/data/forelesningsnotater/w-bertrand-paradox-(probability).pdf
     for i in range(niters):
         match mode:
             case 1:
@@ -47,5 +51,5 @@ def main(niters: int, mode: int) -> None:
 
 if __name__ == "__main__":
 
-    results = main(10000, mode=3)
+    results = main(10000, mode=1)
     print(results)
