69ce456d89c522cc4eba049e8b8dbade163e0b99
vcasellesb <vcasellesb@uoc.edu>
2024-08-16 19:26:33 +0200

    generalized spinner to accept any partition of the circumference (as long as they add up to the whole circumference

--- spinner.py
+++ spinner.py
@@ -3,18 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # example 2.1 https://math.dartmouth.edu/~prob/prob/prob.pdf and exercise 1 (page 52)
-NITERATIONS = 1000000
-r = 1
-CIRCUMFERENCE = 2 * np.pi * r
-
-lwr_bounds_1_2 = 0 * 1/2 * CIRCUMFERENCE
-upr_bounds_1_2 = lwr_bounds_1_2 + 1/2 * CIRCUMFERENCE
-lwr_bounds_1_3 = upr_bounds_1_2
-upr_bounds_1_3 = lwr_bounds_1_3 + 1/3 * CIRCUMFERENCE
-lwr_bounds_1_6 = upr_bounds_1_3
-upr_bounds_1_6 = lwr_bounds_1_6 + 1/6 * CIRCUMFERENCE
-
-assert np.round(upr_bounds_1_6, 7) == np.round(CIRCUMFERENCE, 7), f'{upr_bounds_1_6 = } \n {CIRCUMFERENCE = }'
 
 def between(x: float, lwr_bound: float, upr_bound: float) -> bool:
     return lwr_bound <= x < upr_bound
@@ -43,23 +31,27 @@ def get_height(width: float, p: float):
     return p / width
 
 
-def simulate(NITERATIONS: int) -> Dict[str, int]: 
+def simulate(*bounds, CIRCUMFERENCE: float, NITERATIONS: int) -> Dict[str, int]:
+    bounds = [convert_to_float(b) for b in bounds]
     
-    results = {
-        '1/2': 0,
-        '1/3': 0,
-        '1/6': 0
-    }
-
+    assert np.round(sum(bounds), 10) == 1, sum(bounds)
+    if bounds[0] != 0:
+        bounds = [0] + bounds
+    ps = list(np.array(bounds[1:]))
+    bounds = sorted(list(np.cumsum(np.array(bounds) * CIRCUMFERENCE)), reverse=True)
+    
+    # initialize results
+    results = {str(p): 0 for p in ps}
+    
     for _ in range(NITERATIONS):
         x = np.random.uniform(0, 1) * CIRCUMFERENCE
-
-        results['1/2'] += between(x, lwr_bound=lwr_bounds_1_2, upr_bound=upr_bounds_1_2)
-        results['1/3'] += between(x, lwr_bound=lwr_bounds_1_3, upr_bound=upr_bounds_1_3)
-        results['1/6'] += between(x, lwr_bound=lwr_bounds_1_6, upr_bound=upr_bounds_1_6)
+        for i, b in enumerate(bounds[1:]):
+            this_p = str(ps[::-1][i])            
+            if x > b:
+                results[this_p] += 1
+                break       
 
     results = {k: v/NITERATIONS for k,v in results.items()}
-
     return results
 
 def plot(results: Dict[str, int], fig_path: str):
@@ -79,9 +71,12 @@ def plot(results: Dict[str, int], fig_path: str):
     plt.legend(a.patches, x)
     plt.savefig(fig_path)
 
-def main(NITERATIONS: int, fig_path: str):
-    res = simulate(NITERATIONS)
+def main(*bounds, NITERATIONS: int, fig_path: str, CIRCUMFERENCE: float) -> None:
+    res = simulate(*bounds, CIRCUMFERENCE=CIRCUMFERENCE, NITERATIONS=NITERATIONS)
     plot(res, fig_path)
 
 if __name__ == "__main__":
-    main(NITERATIONS, 'res_spinner.png')
+    NITERATIONS = 1000000
+    r = 1
+    CIRCUMFERENCE = 2 * np.pi * r
+    main('1/3', '1/4', '1/5', '1/6', '1/20', NITERATIONS=NITERATIONS, fig_path='res_spinner.png', CIRCUMFERENCE=CIRCUMFERENCE)
